dde5c9e74a211aea67819bc4b8b5b71e62127960
Mustakeem <00786malikmustakeem@gmail.com>
2026-04-29 19:28:21 +0530

    added get profile

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -143,3 +143,16 @@ exports.login = async (req, res) => {
     });
   }
 };
+
+/* ================= GET PROFILE ================= */
+exports.getProfile = async (req, res) => {
+  try {
+    return res.status(200).json({
+      user: req.user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
